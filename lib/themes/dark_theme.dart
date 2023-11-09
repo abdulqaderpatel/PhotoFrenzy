2ec5f8e98c0f7381954edccb2208418bc28acfe8
abdulqader <abdulqaderpatel2002@gmail.com>
2023-11-09 11:50:09 +0530

    Set light and dark mode theming.

---
+++ lib/themes/dark_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData darkTheme = ThemeData(
+    fontFamily: GoogleFonts.openSans().fontFamily,
+    brightness: Brightness.dark,
+    colorScheme: const ColorScheme.dark(
+        background: Color(0xff15202B),
+        primary: Colors.blue,
+        secondary: Colors.purple,
+        error: Colors.red,
+        errorContainer: Colors.yellow),
+    scaffoldBackgroundColor: Color(0xff151D27),
+    cardColor: Color(0xff242732),
+    dividerColor: Colors.grey[600],
+    appBarTheme: AppBarTheme(backgroundColor: Color(0xff15202B)),
+    iconTheme: const IconThemeData(color: Colors.grey),
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(fontSize: 29,color: Colors.white,fontWeight: FontWeight.w600),
+      displayMedium: TextStyle(fontSize: 25, color: Colors.white54,fontWeight: FontWeight.w600),
+      displaySmall: TextStyle(fontSize: 22, color: Colors.white30,fontWeight: FontWeight.w500),
+      bodyLarge: TextStyle(fontSize: 17,color: Colors.white,fontWeight: FontWeight.w500),
+      bodyMedium: TextStyle(fontSize: 15,color:Color(0xff505863)),
+      bodySmall: TextStyle(fontSize: 13,color: Color(0xff505863)),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.black, // FAB background color
+    ));
---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData lightTheme = ThemeData(
+    fontFamily: GoogleFonts.openSans().fontFamily,
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light(
+        background: Colors.white,
+        primary: Colors.blue,
+        secondary: Colors.purple,
+        error: Colors.red,
+        errorContainer: Colors.yellow),
+    scaffoldBackgroundColor: Colors.white,
+    cardColor: Color(0xffD7EAF1),
+    dividerColor: Colors.grey[600],
+    appBarTheme: AppBarTheme(backgroundColor: Color(0xff15202B)),
+    iconTheme: const IconThemeData(color: Colors.grey),
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(fontSize: 29,color: Colors.black,fontWeight: FontWeight.w600),
+      displayMedium: TextStyle(fontSize: 25, color: Colors.black54,fontWeight: FontWeight.w600),
+      displaySmall: TextStyle(fontSize: 22, color: Colors.black26,fontWeight: FontWeight.w500),
+      bodyLarge: TextStyle(fontSize: 17,color: Colors.black,fontWeight: FontWeight.w500),
+      bodyMedium: TextStyle(fontSize: 15,color:Color(0xffB2B5BC),fontWeight: FontWeight.w400),
+      bodySmall: TextStyle(fontSize: 13,color: Color(0xffB2B5BC),fontWeight: FontWeight.w400),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.black, // FAB background color
+    ));
